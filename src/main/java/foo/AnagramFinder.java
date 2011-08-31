d973cd7ee92697388e01c4493cb5c38963c25809
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-08-31 15:10:23 +0200

    fixed test class

--- src/main/java/foo/AnagramFinder.java
+++ src/main/java/foo/AnagramFinder.java
@@ -6,4 +6,5 @@ package foo;
  * @see <a hrerf="http://codekata.pragprog.com/2007/01/kata_six_anagra.html">Kata description of "Prag Dave".</a>
  */
 public class AnagramFinder {
+
 }
--- src/test/java/foo/AnagramFinderTest.java
+++ src/test/java/foo/AnagramFinderTest.java
@@ -1,4 +1,27 @@
 package foo;
 
+/**
+ * Created by IntelliJ IDEA.
+ * User: khoelsch
+ * Date: 31.08.11
+ * Time: 15:07
+ * To change this template use File | Settings | File Templates.
+ */
 public class AnagramFinderTest {
+  private AnagramFinder anaFinder;
+
+  @org.junit.Before
+  public void setUp() throws Exception {
+    anaFinder = new AnagramFinder();
+  }
+
+  @org.junit.After
+  public void tearDown() throws Exception {
+    anaFinder = null;
+  }
+
+  @Test
+  public void testReadFromFile() {
+    Assert
+  }
 }
