020106149002f08db69342ec531108d872c8f9a3
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-08-31 15:03:39 +0200

    Renamed class

---
+++ src/main/java/foo/AnagramFinder.java
@@ -0,0 +1,9 @@
+package foo;
+
+/**
+ * Find anagrams (= String permutations) in a file and print out all anagrams of the same char set.
+ *
+ * @see <a hrerf="http://codekata.pragprog.com/2007/01/kata_six_anagra.html">Kata description of "Prag Dave".</a>
+ */
+public class AnagramFinder {
+}
--- src/main/java/foo/KataAnagram.java
+++
@@ -1,9 +0,0 @@
-package foo;
-
-/**
- * Find anagrams (= String permutations) in a file and print out all anagrams of the same char set.
- *
- * @see <a hrerf="http://codekata.pragprog.com/2007/01/kata_six_anagra.html">Kata description of "Prag Dave".</a>
- */
-public class KataAnagram {
-}
